96ed31d4246381bfa42a1ee950b1c0971d7ea2ec
Ana Perez Santos <a.jperez@yahoo.com>
2020-04-20 17:01:27 -0600

    Create: discounts link

--- spec/features/merchant/dashboard_spec.rb
+++ spec/features/merchant/dashboard_spec.rb
@@ -62,5 +62,13 @@ RSpec.describe 'Merchant Dashboard' do
 
       expect(current_path).to eq("/merchant/orders/#{@order_2.id}")
     end
+
+    it 'I can link to discounts' do
+      visit '/merchant'
+
+      click_link("Manage Discounts")
+
+      expect(current_path).to eq("/merchant/discounts")
+    end
   end
 end
